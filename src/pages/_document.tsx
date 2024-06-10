915ef04d1e84608d822e8859491d30ba29e8d742
Batista Tone <batistatomeoliveira96@gmail.com>
2024-06-10 17:32:12 +0100

    add preview image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,6 +48,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:image" content="./preview.png" />
       </Head>
       <body>
         <Main />
